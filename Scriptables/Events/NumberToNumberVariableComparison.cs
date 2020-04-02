33b000cc171af30cf2cacecd750418f5dfcd2205
Nathan Loudjani <nathan.loudj@gmail.com>
2020-04-02 18:45:17 +0200

    Renamed NumberToNumberVariableComparison to match name standards, fix code style and optimize imports

---
+++ Scriptables/Events/NumberToNumberVariableComparison.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+
+namespace SO
+{
+    public class NumberToNumberVariableComparison : EventExecutionOnMB
+    {
+        public float fixedNumber;
+        public NumberVariable targetVariable;
+
+        public UnityEvent IfVariableIsLower;
+        public UnityEvent IfVariableIsHigher;
+
+        /// <summary>
+        /// Invoke the true or false event stack based on a comparison of your targetVariable and a fixed number
+        /// The comparison only runs when the Raise() is called, it's not monitored in Update or etc.
+        /// </summary>
+        public override void Raise()
+        {
+            if (targetVariable == null)
+            {
+                Debug.Log("No number variable assigned in a fixed number to numberVariable comparison! " +
+                          this.gameObject.name);
+                return;
+            }
+
+            switch (targetVariable)
+            {
+                case FloatVariable floatVariable:
+                {
+                    if (floatVariable.value < fixedNumber)
+                        IfVariableIsLower.Invoke();
+                    else
+                        IfVariableIsHigher.Invoke();
+                    break;
+                }
+                case IntVariable intVariable:
+                {
+                    int fixedNumberRoundedToInt = Mathf.RoundToInt(fixedNumber);
+                    if (intVariable.value < fixedNumberRoundedToInt)
+                        IfVariableIsLower.Invoke();
+                    else
+                        IfVariableIsHigher.Invoke();
+                    break;
+                }
+            }
+        }
+    }
+}
