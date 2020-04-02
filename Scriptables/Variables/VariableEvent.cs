988857b4e0e6a7a77473216c15c68c14fbf7d087
Nathan Loudjani <nathan.loudj@gmail.com>
2020-04-02 19:50:04 +0200

    Create parent VariableEvent for ScriptableObject variables

---
+++ Scriptables/Variables/VariableEvent.cs
@@ -0,0 +1,47 @@
+using System.Collections.Generic;
+
+namespace SO
+{
+    public class VariableEvent<T> : GameEvent
+    {
+        private T value;
+
+        public T Value
+        {
+            get => value;
+            set
+            {
+                this.value = value;
+                Raise();
+            }
+        }
+
+        public void Set(VariableEvent<T> variableEvent)
+        {
+            Value = variableEvent.value;
+        }
+
+        protected bool Equals(VariableEvent<T> other)
+        {
+            return base.Equals(other) && EqualityComparer<T>.Default.Equals(value, other.value);
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (ReferenceEquals(null, obj)) return false;
+            if (ReferenceEquals(this, obj)) return true;
+            if (obj.GetType() != GetType() && obj.GetType() != typeof(T)) return false;
+            if (obj.GetType() == typeof(T))
+                return EqualityComparer<T>.Default.Equals(obj is T objAsT ? objAsT : default, Value);
+            return Equals((VariableEvent<T>) obj);
+        }
+
+        public override int GetHashCode()
+        {
+            unchecked
+            {
+                return (base.GetHashCode() * 397) ^ EqualityComparer<T>.Default.GetHashCode(Value);
+            }
+        }
+    }
+}
