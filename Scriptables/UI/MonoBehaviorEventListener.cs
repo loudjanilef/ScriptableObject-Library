1e55db49b4474d37f900c0edea661408b06932bb
Nathan Loudjani <nathan.loudj@gmail.com>
2020-04-04 19:00:03 +0200

    Create MonoBehaviorEventListener to listen for GameEvent in GameObjects

---
+++ Scriptables/UI/MonoBehaviorEventListener.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+
+namespace SO.UI
+{
+    public abstract class MonoBehaviorEventListener : MonoBehaviour, IGameEventListener
+    {
+        /// <summary>
+        /// Determine if the Response should be called as soon as the game event is enabled
+        /// </summary>
+        public bool respondOnEnable;
+
+        /// <summary>
+        /// Determine if the Response should be called as soon as the game event is disabled (you never know)
+        /// </summary>
+        public bool respondOnDisable;
+
+        protected virtual void OnEnable()
+        {
+            if (respondOnEnable)
+            {
+                Response();
+            }
+        }
+
+        protected virtual void OnDisable()
+        {
+            if (respondOnDisable)
+            {
+                Response();
+            }
+        }
+
+        public abstract void Response();
+    }
+}
