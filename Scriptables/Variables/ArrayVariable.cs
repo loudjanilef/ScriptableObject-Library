699675362033a4287024464fb296abdffdce7a59
Nathan Loudjani <nathan.loudj@gmail.com>
2020-04-04 19:20:55 +0200

    Improve Array and List support

---
+++ Scriptables/Variables/ArrayVariable.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using UnityEngine;
+
+namespace SO
+{
+    public class ArrayVariable<T> : VariableEvent<T[]>
+    {
+        public int Length()
+        {
+            return Value.Length;
+        }
+
+        public void Empty()
+        {
+            Value = null;
+        }
+
+        public void InitLength(int v)
+        {
+            Value = new T[v];
+        }
+
+        public void ReplaceAt(T newValue, int position)
+        {
+            if (position >= 0 && position < Value.Length)
+            {
+                Value[position] = newValue;
+                Raise();
+            }
+            else
+            {
+                throw new ArgumentOutOfRangeException(nameof(position));
+            }
+        }
+    }
+}
---
+++ Scriptables/Variables/ListVariable.cs
@@ -0,0 +1,37 @@
+using System.Collections.Generic;
+
+namespace SO
+{
+    public class ListVariable<T> : VariableEvent<List<T>>
+    {
+        public void Add(T element)
+        {
+            Value.Add(element);
+            Raise();
+        }
+
+        public void Insert(int index, T item)
+        {
+            Value.Insert(index, item);
+            Raise();
+        }
+
+        public void RemoveAt(int index)
+        {
+            Value.RemoveAt(index);
+            Raise();
+        }
+
+        public void Remove(T element)
+        {
+            Value.Remove(element);
+            Raise();
+        }
+
+        public void Clear()
+        {
+            Value.Clear();
+            Raise();
+        }
+    }
+}
