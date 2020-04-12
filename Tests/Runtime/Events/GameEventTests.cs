df4659b7feeee3b7ce8891752d72b527fb7d65d8
Nathan Loudjani <nathan.loudj@gmail.com>
2020-04-12 17:59:18 +0200

    Add first test

---
+++ Tests/Runtime/Events/GameEventTests.cs
@@ -0,0 +1,38 @@
+﻿using NUnit.Framework;
+using SO;
+using UnityEngine;
+
+namespace Tests
+{
+    public class GameEventTests
+    {
+        private class TestGameEventListener : IGameEventListener
+        {
+            public int ResponseCallNumber;
+
+            public void Response()
+            {
+                ResponseCallNumber++;
+            }
+        }
+
+        [Test]
+        public void GameEventNotifiesOnlyRegisteredListeners()
+        {
+            GameEvent gameEvent = ScriptableObject.CreateInstance<GameEvent>();
+            TestGameEventListener listener = new TestGameEventListener();
+            Assert.AreEqual(0, listener.ResponseCallNumber);
+
+            gameEvent.Raise();
+            Assert.AreEqual(0, listener.ResponseCallNumber);
+
+            gameEvent.Register(listener);
+            gameEvent.Raise();
+            Assert.AreEqual(1, listener.ResponseCallNumber);
+
+            gameEvent.UnRegister(listener);
+            gameEvent.Raise();
+            Assert.AreEqual(1, listener.ResponseCallNumber);
+        }
+    }
+}
