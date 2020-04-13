47421f0333a7162b343f323c055d35eb9eb428e9
Nathan Loudjani <nathan.loudj@gmail.com>
2020-04-13 16:26:10 +0200

    Check that raise is called in list and array variable tests

--- Tests/Runtime/Events/Variables/ArrayVariableTests.cs
+++ Tests/Runtime/Events/Variables/ArrayVariableTests.cs
@@ -11,20 +11,30 @@ namespace Tests
             SimpleArrayVariableEvent arrayVariableEvent = ScriptableObject.CreateInstance<SimpleArrayVariableEvent>();
             Assert.IsNull(arrayVariableEvent.Value);
 
+            SimpleGameEventListener listener = new SimpleGameEventListener();
+            arrayVariableEvent.Register(listener);
+            Assert.AreEqual(0, listener.ResponseCallNumber);
+
             arrayVariableEvent.InitLength(5);
             Assert.IsNotNull(arrayVariableEvent.Value);
             Assert.AreEqual(5, arrayVariableEvent.Value.Length);
+            Assert.AreEqual(1, listener.ResponseCallNumber);
         }
 
         [Test]
         public void Empty()
         {
             SimpleArrayVariableEvent arrayVariableEvent = ScriptableObject.CreateInstance<SimpleArrayVariableEvent>();
+            SimpleGameEventListener listener = new SimpleGameEventListener();
+
             arrayVariableEvent.Value = new int[5];
             Assert.IsNotNull(arrayVariableEvent.Value);
+            arrayVariableEvent.Register(listener);
+            Assert.AreEqual(0, listener.ResponseCallNumber);
 
             arrayVariableEvent.Empty();
             Assert.IsNull(arrayVariableEvent.Value);
+            Assert.AreEqual(1, listener.ResponseCallNumber);
         }
 
         [Test]
@@ -39,10 +49,15 @@ namespace Tests
         public void ReplaceAt()
         {
             SimpleArrayVariableEvent arrayVariableEvent = ScriptableObject.CreateInstance<SimpleArrayVariableEvent>();
-            arrayVariableEvent.Value = new int[3];
+            SimpleGameEventListener listener = new SimpleGameEventListener();
 
+            arrayVariableEvent.Value = new int[3];
             Assert.AreEqual(new[] {0, 0, 0}, arrayVariableEvent.Value);
+            arrayVariableEvent.Register(listener);
+            Assert.AreEqual(0, listener.ResponseCallNumber);
+
             arrayVariableEvent.ReplaceAt(1, 1);
+            Assert.AreEqual(1, listener.ResponseCallNumber);
             Assert.AreEqual(new[] {0, 1, 0}, arrayVariableEvent.Value);
         }
     }
--- Tests/Runtime/Events/Variables/ListVariableTests.cs
+++ Tests/Runtime/Events/Variables/ListVariableTests.cs
@@ -18,8 +18,12 @@ namespace Tests
         public void Init()
         {
             SimpleListVariableEvent listVariableEvent = ScriptableObject.CreateInstance<SimpleListVariableEvent>();
+            SimpleGameEventListener listener = new SimpleGameEventListener();
+            listVariableEvent.Register(listener);
+            Assert.AreEqual(0, listener.ResponseCallNumber);
             Assert.IsNull(listVariableEvent.Value);
             listVariableEvent.Init();
+            Assert.AreEqual(1, listener.ResponseCallNumber);
             Assert.IsNotNull(listVariableEvent.Value);
         }
 
@@ -27,18 +31,26 @@ namespace Tests
         public void InitWithCapacity()
         {
             SimpleListVariableEvent listVariableEvent = ScriptableObject.CreateInstance<SimpleListVariableEvent>();
+            SimpleGameEventListener listener = new SimpleGameEventListener();
+            listVariableEvent.Register(listener);
+            Assert.AreEqual(0, listener.ResponseCallNumber);
             Assert.IsNull(listVariableEvent.Value);
             listVariableEvent.Init(6);
             Assert.IsNotNull(listVariableEvent.Value);
             Assert.AreEqual(6, listVariableEvent.Value.Capacity);
+            Assert.AreEqual(1, listener.ResponseCallNumber);
         }
 
         [Test]
         public void InitWithValues()
         {
             SimpleListVariableEvent listVariableEvent = ScriptableObject.CreateInstance<SimpleListVariableEvent>();
+            SimpleGameEventListener listener = new SimpleGameEventListener();
+            listVariableEvent.Register(listener);
+            Assert.AreEqual(0, listener.ResponseCallNumber);
             Assert.IsNull(listVariableEvent.Value);
             listVariableEvent.Init(new[] {2, 4});
+            Assert.AreEqual(1, listener.ResponseCallNumber);
             Assert.IsNotNull(listVariableEvent.Value);
             Assert.AreEqual(new[] {2, 4}, listVariableEvent.Value);
         }
@@ -47,9 +59,13 @@ namespace Tests
         public void Add()
         {
             SimpleListVariableEvent listVariableEvent = ScriptableObject.CreateInstance<SimpleListVariableEvent>();
+            SimpleGameEventListener listener = new SimpleGameEventListener();
             listVariableEvent.Value = new List<int>();
+            listVariableEvent.Register(listener);
+            Assert.AreEqual(0, listener.ResponseCallNumber);
             Assert.AreEqual(0, listVariableEvent.Value.Count);
             listVariableEvent.Add(3);
+            Assert.AreEqual(1, listener.ResponseCallNumber);
             Assert.AreEqual(1, listVariableEvent.Value.Count);
             Assert.AreEqual(3, listVariableEvent.Value[0]);
         }
@@ -58,9 +74,13 @@ namespace Tests
         public void Insert()
         {
             SimpleListVariableEvent listVariableEvent = ScriptableObject.CreateInstance<SimpleListVariableEvent>();
+            SimpleGameEventListener listener = new SimpleGameEventListener();
             listVariableEvent.Value = new List<int>(new[] {0, 0, 0, 0});
+            listVariableEvent.Register(listener);
+            Assert.AreEqual(0, listener.ResponseCallNumber);
             Assert.AreEqual(4, listVariableEvent.Value.Count);
             listVariableEvent.Insert(3, 1);
+            Assert.AreEqual(1, listener.ResponseCallNumber);
             Assert.AreEqual(5, listVariableEvent.Value.Count);
             Assert.AreEqual(new[] {0, 0, 0, 1, 0}, listVariableEvent.Value);
         }
@@ -69,9 +89,13 @@ namespace Tests
         public void RemoveAt()
         {
             SimpleListVariableEvent listVariableEvent = ScriptableObject.CreateInstance<SimpleListVariableEvent>();
+            SimpleGameEventListener listener = new SimpleGameEventListener();
             listVariableEvent.Value = new List<int>(new[] {1, 2, 3, 4});
+            listVariableEvent.Register(listener);
+            Assert.AreEqual(0, listener.ResponseCallNumber);
             Assert.AreEqual(4, listVariableEvent.Value.Count);
             listVariableEvent.RemoveAt(1);
+            Assert.AreEqual(1, listener.ResponseCallNumber);
             Assert.AreEqual(3, listVariableEvent.Value.Count);
             Assert.AreEqual(new[] {1, 3, 4}, listVariableEvent.Value);
         }
@@ -80,9 +104,13 @@ namespace Tests
         public void Remove()
         {
             SimpleListVariableEvent listVariableEvent = ScriptableObject.CreateInstance<SimpleListVariableEvent>();
+            SimpleGameEventListener listener = new SimpleGameEventListener();
             listVariableEvent.Value = new List<int>(new[] {1, 2, 3, 4});
+            listVariableEvent.Register(listener);
+            Assert.AreEqual(0, listener.ResponseCallNumber);
             Assert.AreEqual(4, listVariableEvent.Value.Count);
             listVariableEvent.Remove(3);
+            Assert.AreEqual(1, listener.ResponseCallNumber);
             Assert.AreEqual(3, listVariableEvent.Value.Count);
             Assert.AreEqual(new[] {1, 2, 4}, listVariableEvent.Value);
         }
@@ -90,9 +118,13 @@ namespace Tests
         public void Clear()
         {
             SimpleListVariableEvent listVariableEvent = ScriptableObject.CreateInstance<SimpleListVariableEvent>();
+            SimpleGameEventListener listener = new SimpleGameEventListener();
             listVariableEvent.Value = new List<int>(new[] {1, 2, 3, 4});
+            listVariableEvent.Register(listener);
+            Assert.AreEqual(0, listener.ResponseCallNumber);
             Assert.AreEqual(4, listVariableEvent.Value.Count);
             listVariableEvent.Clear();
+            Assert.AreEqual(1, listener.ResponseCallNumber);
             Assert.AreEqual(0, listVariableEvent.Value.Count);
         }
     }
